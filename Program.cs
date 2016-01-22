9878b0e9e409adbf39ac49337d2201f827c3d8c4
zhaziraumirkalykova <Zhazirataldik@mail.ru>
2016-01-23 02:27:27 +0600

    program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        class Program 
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            int[,] b = new int[3, 4];
+            int[,,] c = new int[2, 3, 4];
+            int[] a1 = { 1, 2, 3, 4, 5 };
+
+        }
+    }
+}
