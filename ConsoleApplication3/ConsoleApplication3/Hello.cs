c442a4a1e942b209ebb2bac1df9a629b75fbd992
zhaziraumirkalykova <Zhazirataldik@mail.ru>
2016-01-30 13:07:27 +0600

    HelloWorld!

---
+++ ConsoleApplication3/ConsoleApplication3/Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// A Hello World! program in c#.
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            // keep the console window open in debug mode
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
