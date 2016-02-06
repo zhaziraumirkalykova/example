10417ac96807a6a61367bb6bfeef84bee77af7f9
zhaziraumirkalykova <Zhazirataldik@mail.ru>
2016-02-06 19:29:42 +0600

    lab2-task1

---
+++ ConsoleApplication11/ConsoleApplication11/stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stackii(@"C:\Users\HP\Downloads\far2.0\PT");
+        }
+        static void Stackii(string path)
+        {
+            Stack<string> dirs = new Stack<string>(20);// create the stack
+            Console.WriteLine(path + "/" + Directory.GetFiles(path).Length);// quantity of file in the path 
+            dirs.Push(path);// add the path for initual file
+
+            while (dirs.Count > 0)// do the cycle while the stack gets 0
+            {
+                string currentDir = dirs.Pop();// takes the path of the last file 
+                string[] subDirs = Directory.GetDirectories(currentDir);// into the massiv gets the files from last folder 
+                foreach (string str in subDirs)
+                {
+                    Console.WriteLine(str + ": " + Directory.GetFiles(str).Length);// quantity of files in each folder
+                    dirs.Push(str);//path of the files in the stack
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
